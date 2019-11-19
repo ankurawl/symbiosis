d5ef6018894c0909b98ffe4b4b8480a7c2961ce5
AnkurA <ankur.awl@gmail.com>
2019-11-19 00:21:55 -0800

    updated the upload-lambda with actual code used in lambda

--- upload-lambda.py
+++ upload-lambda.py
@@ -1,20 +1,26 @@
+import json
 import boto3
 from io import BytesIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
-#for bucket in s3.buckets.all():
-#    print(bucket.name)
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    #for bucket in s3.buckets.all():
+    #    print(bucket.name)
+    portfolio_bucket = s3.Bucket('portfolio.elkong.com')
+    build_bucket = s3.Bucket('portfoliobuild.elkong.com')
 
-portfolio_bucket = s3.Bucket('portfolio.elkong.com')
-build_bucket = s3.Bucket('portfoliobuild.elkong.com')
+    portfolio_buildzip_rawdata = BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_buildzip_rawdata)
+    myzip = zipfile.ZipFile(portfolio_buildzip_rawdata)
 
-portfolio_buildzip_rawdata = BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip',portfolio_buildzip_rawdata)
-myzip = zipfile.ZipFile(portfolio_buildzip_rawdata)
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-for nm in myzip.namelist():
-    obj = myzip.open(nm)
-    portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-    portfolio_bucket.Object(nm).Acl().put(ACL='public-read') 
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
