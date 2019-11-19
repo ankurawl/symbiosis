d354827046f01d1372f0770d6209b3ee44954afb
AnkurA <ankur.awl@gmail.com>
2019-11-18 23:43:53 -0800

    Added python script to unzip portfolio from build file and upload to S3

---
+++ upload-lambda.py
@@ -0,0 +1,20 @@
+import boto3
+from io import BytesIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+#for bucket in s3.buckets.all():
+#    print(bucket.name)
+
+portfolio_bucket = s3.Bucket('portfolio.elkong.com')
+build_bucket = s3.Bucket('portfoliobuild.elkong.com')
+
+portfolio_buildzip_rawdata = BytesIO()
+build_bucket.download_fileobj('portfoliobuild.zip',portfolio_buildzip_rawdata)
+myzip = zipfile.ZipFile(portfolio_buildzip_rawdata)
+
+for nm in myzip.namelist():
+    obj = myzip.open(nm)
+    portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+    portfolio_bucket.Object(nm).Acl().put(ACL='public-read') 
