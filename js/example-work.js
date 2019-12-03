e6e3641a060cc81bdb8cf0802421db7ea696ba35
AnkurA <ankur.awl@gmail.com>
2019-12-02 18:28:35 -0800

    added npm webpack and react based modification to index.html

---
+++ js/example-work.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class ExampleWork extends React.Component {
+  render() {
+    return (
+      <section className="section section--alignCentered section--description">
+
+      { this.props.work.map( (example,idx) => {
+        return (<ExampleWorkBubble example={example} key={idx} />)
+      } ) }
+
+      </section>
+    )
+  }
+}
+
+class ExampleWorkBubble extends React.Component {
+  render() {
+    let exmpl = this.props.example
+    return (
+      <div className="section__exampleWrapper">
+        <div className="section__example">
+          <img  alt= { exmpl.image.desc }
+                className="section__exampleImage"
+                src={ exmpl.image.src } />
+          <dl className="color--cloud">
+            <dt className="section__exampleTitle section__text--centered ">
+                {exmpl.title}
+            </dt>
+            <dd></dd>
+          </dl>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ExampleWork;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Code example",
+    'image': {
+      'desc': "Code example",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Product mgmt example",
+    'image': {
+      'desc': "Product management example",
+      'src': "images/example3.png",
+      'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+                  https://www.flickr.com/photos/37287295@N00/2540855181 `
+    }
+  },
+  {
+    'title': "Design example",
+    'image': {
+      'desc': "Design example",
+      'src': "images/example2.png",
+      'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+                  https://www.flickr.com/photos/ssoosay/4097410999`
+    }
+  }
+]
 
-console.log("Webpack works");
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
